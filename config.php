13283afc948039ed3b802e9fa8bfb641edcc71f4
o0xd4n4-69 <65133233+o0xd4n4-69@users.noreply.github.com>
2020-05-11 02:45:59 +0700

    Add files via upload

---
+++ config.php
@@ -0,0 +1,3 @@
+<?php
+$userid = "EC-UserId-61936";
+$ua = "Mozilla/5.0 (Linux; Android 7.1.2; Redmi 3 Build/NJH47F; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/60.0.3112.78 Mobile Safari/537.36";
